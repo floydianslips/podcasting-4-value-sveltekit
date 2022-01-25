e12eb303e7a0d24eb98875768232f6bcdbc42639
Joshua Dennis <afraidofemail@gmail.com>
2022-01-25 13:44:56 -0800

    test netlify-form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,56 +1,23 @@
-<script>
-	import Callout from '$lib/components/Callout.svelte';
+<script context="module">
+	export const prerender = true;
 </script>
 
-<svelte:head>
-	<title>Contact</title>
-</svelte:head>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h1>Contact</h1>
-
-<Callout>What's on your mind?</Callout>
-
-<form name="contact" data-netlify-recaptcha="true" netlify>
-	<!-- <input type="hidden" name="contact" value="p4v-contact" /> -->
-
-	<div class="form-section">
-		<label for="name">Name</label>
-		<input type="text" id="name" placeholder="First name" />
-	</div>
-
-	<div class="form-section">
-		<label for="email">Email</label>
-		<input type="email" id="email" placeholder="Email address" />
-	</div>
-
-	<div class="form-section">
-		<label for="message">Message</label>
-		<input type="text" id="message" placeholder="What's up?" />
-	</div>
-	<!-- 
-  <fieldset>
-    <legend>
-      Which option?
-    </legend> -->
-
-	<!-- <div> -->
-	<!-- <input type="radio" name="s" id="s1" value="s1"> -->
-	<!-- <label for="s1">Option 1</label> -->
-	<!-- </div> -->
-	<!-- <div> -->
-	<!-- <input type="radio" name="s" id="s2" value="s2"> -->
-	<!-- <label for="s2">Option 2</label> -->
-	<!-- </div> -->
-	<!-- <div> -->
-	<!-- <input type="radio" name="s" id="s3" value="s3"> -->
-	<!-- <label for="s3">Option 3</label> -->
-	<!-- </div> -->
-	<!-- </fieldset> -->
-
-	<!-- <div class="form-section">
-    <input type="checkbox" id="c1" >
-    <label for="c1">Sign me up for something!</label>
-  </div> -->
-
-	<button type="submit">Send</button>
+<form name="test" method="post" netlify netlify-honeypot="bot-field">
+	<input type="hidden" name="form-name" value="test" />
+	<input type="text" name="bot-field" />
+	<p>
+		<label>Your Name: <input type="text" name="name" /></label>
+	</p>
+	<p>
+		<label>Your Email: <input type="email" name="email" /></label>
+	</p>
+	<p>
+		<label>Message: <textarea name="message" /></label>
+	</p>
+	<p>
+		<button type="submit">Send</button>
+	</p>
 </form>
