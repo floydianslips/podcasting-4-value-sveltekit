0ed801b84194a9e08788714f023bece0aa9adcc3
Joshua Dennis <afraidofemail@gmail.com>
2022-01-25 20:26:17 -0800

    test netlify-form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -11,15 +11,15 @@
 <form name="contact" method="post" netlify netlify-honeypot="bot-field">
 	<!-- <input type="hidden" name="form-name" value="contact" /> -->
 	<!-- <input type="text" name="bot-field" /> -->
-	<div class="form-section">
+	<p class="form-section">
 		<label>Your Name: <input type="text" name="name" /></label>
-	</div>
-	<div class="form-section">
+	</p>
+	<p class="form-section">
 		<label>Your Email: <input type="email" name="email" /></label>
-	</div>
-	<div class="form-section">
+	</p>
+	<p class="form-section">
 		<label>Message: <textarea name="message" /></label>
-	</div>
+	</p>
 	<p>
 		<button type="submit">Send</button>
 	</p>
