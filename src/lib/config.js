d0ca4219a66ae54899978bf76c4ebdd182836f72
Joshua Dennis <afraidofemail@gmail.com>
2022-01-25 20:49:44 -0800

    removed blog/about from nav

--- src/lib/config.js
+++ src/lib/config.js
@@ -10,14 +10,14 @@ export const postsPerPage = 10;
 
 // Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
 export const navItems = [
-	{
-		title: 'Blog',
-		route: '/blog'
-	},
-	{
-		title: 'About',
-		route: '/about'
-	},
+	// {
+	// 	title: 'Blog',
+	// 	route: '/blog'
+	// },
+	// {
+	// 	title: 'About',
+	// 	route: '/about'
+	// },
 	{
 		title: 'Contact',
 		route: '/contact'
