ec6e33f1e83da40179a7df68a0805256a6d3dbe4
Joshua Dennis <afraidofemail@gmail.com>
2022-01-25 20:23:31 -0800

    test netlify-form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -8,7 +8,7 @@
 
 <h1>Contact</h1>
 
-<form name="contact" method="post" netlify data-netlify-recaptcha="true">
+<form name="contact" method="post" netlify netlify-honeypot="bot-field">
 	<!-- <input type="hidden" name="form-name" value="contact" /> -->
 	<!-- <input type="text" name="bot-field" /> -->
 	<div class="form-section">
