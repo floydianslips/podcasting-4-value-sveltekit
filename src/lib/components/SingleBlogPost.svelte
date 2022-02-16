34cf5325ccb82cf4ca2cb18fd4a37da26f061fdd
Joshua Dennis <afraidofemail@gmail.com>
2022-02-16 15:13:43 -0800

    adjusted date cms

--- src/lib/components/SingleBlogPost.svelte
+++ src/lib/components/SingleBlogPost.svelte
@@ -38,21 +38,7 @@
 	<title>Blog</title>
 	<meta data-key="description" name="description" content={siteDescription} />
 </svelte:head>
-<!-- <Scroller top={0.2} bottom={0.8} bind:index bind:offset bind:progress>
-	<div slot="background">
-		<p>
-			This is the background content. It will stay fixed in place while the foreground scrolls over
-			the top.
-		</p>
 
-		<p>Section {index + 1} is currently active.</p>
-	</div> -->
-
-<!-- <section>This is the first section.</section> -->
-<!-- <section>This is the second section.</section> -->
-<!-- <section>This is the third section.</section> -->
-<!-- <div slot="foreground"> -->
-<!-- {#each posts as post} -->
 <a href="/blog/{posts[index]?.slug}">
 	<article>
 		<img src={posts[index]?.coverImage} alt="" height={imgWidth} />
